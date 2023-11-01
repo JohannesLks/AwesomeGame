3db32c1ff75a6b0e1e2ac80805b878f56723ffc1
JohannesLks <74617774+JohannesLks@users.noreply.github.com>
2023-11-01 21:05:45 +0100

    media Ordner,settings, sprites, power ups

---
+++ settings.py
@@ -0,0 +1,7 @@
+# Constants for the game
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 563
+PLAYER_SPEED = 5
+ENEMY_SPAWN_RATE = 60  # Lower number means more frequent spawns
+POWER_UP_RATE = 500    # Lower number means more frequent power-ups
+SCORE_MULTIPLIER = 2   # For X2-Boost power-up
